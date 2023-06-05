7e8787befd7a9bd21391b57741b004a1bee8cbb8
roseanprime <roseanprime1999@gmail.com>
2023-06-05 17:05:14 +0100

    google books api working on app

--- models/Book.model.js
+++ models/Book.model.js
@@ -1,43 +1,42 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require("mongoose");
 
-
-const bookSchema = new Schema({
-
+const bookSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
     },
     author: [String],
     publisher: {
-        type: String,
-        required: true,
+      type: String,
     },
     image: {
-        type: String,
-        default: 'https://wallpaper.dog/large/817497.jpg'
+      type: String,
+      default: "https://wallpaper.dog/large/817497.jpg",
     },
     description: String,
     price: {
-        type: Number,
-        required: true,
+      type: Number,
     },
     currency: {
-        type: String,
-        default: '€',
+      type: String,
+      default: "€",
     },
-    review: [{
+    review: [
+      {
         type: Schema.Types.ObjectId,
-        ref: 'Review',
-    }],
+        ref: "Review",
+      },
+    ],
     owner: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    }
-}, {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-const Book = model("Book", bookSchema)
+const Book = model("Book", bookSchema);
 
-
-module.exports = Book
+module.exports = Book;
--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,142 +1,141 @@
-const express = require('express')
-const router = express.Router()
-const axios = require('axios')
-const Book = require('./../models/Book.model')
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const Book = require("./../models/Book.model");
 
+//CREATE BOOK
+router.post("/create", (req, res) => {
+  /* const id = req.payload._id */
 
+  const { title, author, publisher, image, description, price, currency } =
+    req.body;
 
-
-//CREATE BOOK 
-router.post('/create', (req, res) => {
-
-    /* const id = req.payload._id */
-
-    const { title, author, publisher, image, description, price, currency } = req.body
-
-    Book
-        .create({ title, author, publisher, image, description, price, currency/* , owner: id  */})
-        .then(book => res.json(book))
-        .catch(err => res.status(500).json({ code: 500, message: 'Could not create book', err }))
-})
-
-
+  Book.create({
+    title,
+    author,
+    publisher,
+    image,
+    description,
+    price,
+    currency /* , owner: id  */,
+  })
+    .then((book) => res.json(book))
+    .catch((err) =>
+      res.status(500).json({ code: 500, message: "Could not create book", err })
+    );
+});
 
 //GET BOOKS LIST
-router.get('/list', (req, res) => {
+router.get("/list", async (req, res) => {
+  const allBooks = await Book.find();
+  res.status(200).json(allBooks);
+});
 
-    Book
-        .find({ accepted: true })
-        .populate('owner review')
-        .sort({createdAt: 1})
-        .then(book => res.json(book))
-        .catch(err => res.status(500).json({ code: 500, message: 'Book list not found', err }))
-})
+//READ BOOK
+router.get("/details/:book_id", (req, res) => {
+  const { book_id } = req.params;
 
+  Book.findById(book_id)
+    .populate("owner review")
+    .then((book) => res.json(book))
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ code: 500, message: "Book details not found", err })
+    );
+});
 
+//EDIT BOOK
+router.put("/:book_id", (req, res) => {
+  const { book_id } = req.params;
+  const { title, description, price, currency, image } = req.body;
 
-//READ BOOK 
-router.get('/details/:book_id', (req, res) => {
-
-    const { book_id } = req.params
-
-    Book
-        .findById(book_id)
-        .populate('owner review')
-        .then(book => res.json(book))
-        .catch(err => res.status(500).json({ code: 500, message: 'Book details not found', err }))
-})
-
-
-
-//EDIT BOOK 
-router.put('/:book_id', (req, res) => {
-
-    const { book_id } = req.params
-    const { title, description, price, currency, image } = req.body
-
-    Book
-        .findByIdAndUpdate(book_id, { title, description, price, currency, image }, { new: true })
-        .then(book => res.json(book))
-        .catch(err => res.status(500).json({ code: 500, message: 'Could not edit book', err }))
-})
+  Book.findByIdAndUpdate(
+    book_id,
+    { title, description, price, currency, image },
+    { new: true }
+  )
+    .then((book) => res.json(book))
+    .catch((err) =>
+      res.status(500).json({ code: 500, message: "Could not edit book", err })
+    );
+});
 
 //GET BOOKS NOT CONFIRMED LIST
-router.get('/confirm/list', (req, res) => {
-
-    Book
-        .find({ accepted: false })
-        .select(' _id title image')
-        .then(book => res.json(book))
-        .catch(err => res.status(500).json({ code: 500, message: 'Book to confirm not found', err }))
-})
+router.get("/confirm/list", (req, res) => {
+  Book.find({ accepted: false })
+    .select(" _id title image")
+    .then((book) => res.json(book))
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ code: 500, message: "Book to confirm not found", err })
+    );
+});
 
 /*CONFIRM BOOK TO SELL*/
 
-router.put('/confirm/:book_id', (req, res) => {
+router.put("/confirm/:book_id", (req, res) => {
+  const { book_id } = req.params;
+  const { accepted } = req.body;
+  console.log(req.body, " -----req.body");
+  Book.findByIdAndUpdate(book_id, { accepted }, { new: true })
+    .then((book) => res.json(book))
+    .catch((err) =>
+      res.status(500).json({ code: 500, message: "Could not edit book", err })
+    );
+});
 
-    const { book_id } = req.params
-    const { accepted } = req.body
-    console.log(req.body, " -----req.body")
-    Book
-        .findByIdAndUpdate(book_id, { accepted }, { new: true })
-        .then(book => res.json(book))
-        .catch(err => res.status(500).json({ code: 500, message: 'Could not edit book', err }))
-})
+//DELETE BOOK
+router.delete("/:book_id", (req, res) => {
+  const { book_id } = req.params;
 
-//DELETE BOOK 
-router.delete('/:book_id', (req, res) => {
-
-    const { book_id } = req.params
-
-    Book
-        .findByIdAndDelete(book_id)
-        .then(book => res.json(book))
-        .catch(err => res.status(500).json({ code: 500, message: 'Could not delete book', err }))
-})
-
-
+  Book.findByIdAndDelete(book_id)
+    .then((book) => res.json(book))
+    .catch((err) =>
+      res.status(500).json({ code: 500, message: "Could not delete book", err })
+    );
+});
 
 //GOOGLE BOOKS API ROUTE
-router.get('/api-books', async (req, res) => {
-    const { search } = req.body
-    const searchQuery = search.replace(" ", "+")
+router.get("/api-books/:bookName", async (req, res) => {
+  const { bookName } = req.params;
+  /*   const searchQuery = bookName.replace("%20", "+"); */
 
-    const searchResult = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&key=${process.env.API_KEY}`) 
+  const searchResult = await axios.get(
+    `https://www.googleapis.com/books/v1/volumes?q=${bookName}&key=${process.env.API_KEY}`
+  );
 
-    const bookTitles = []
-    const dbBooks = await Book.find()
-    dbBooks.map((book)=>{
-bookTitles.push(book.title)
-    })
+  const bookTitles = [];
+  const dbBooks = await Book.find();
+  dbBooks.map((book) => {
+    bookTitles.push(book.title);
+  });
 
-    const book = searchResult.data.items[0]
+  const book = searchResult.data.items[0];
 
-
-    if (!bookTitles.includes(book.volumeInfo.title)){
-
-    if (!book.volumeInfo.imageLinks){
-        Book.create({ 
-            title: book.volumeInfo.title, 
-            author: book.volumeInfo.authors , 
-            publisher: book.volumeInfo.publisher , 
-            description: book.volumeInfo.description , 
-            /* , owner: id  */})
-    }else{
-        Book.create({ 
-            title: book.volumeInfo.title, 
-            author: book.volumeInfo.authors , 
-            publisher: book.volumeInfo.publisher , 
-            image: book.volumeInfo.imageLinks.thumbnail , 
-            description: book.volumeInfo.description , 
-            /* , owner: id  */})
+  if (!bookTitles.includes(book.volumeInfo.title)) {
+    if (!book.volumeInfo.imageLinks) {
+      Book.create({
+        title: book.volumeInfo.title,
+        author: book.volumeInfo.authors,
+        publisher: book.volumeInfo.publisher,
+        description: book.volumeInfo.description,
+        /* , owner: id  */
+      });
+    } else {
+      Book.create({
+        title: book.volumeInfo.title,
+        author: book.volumeInfo.authors,
+        publisher: book.volumeInfo.publisher,
+        image: book.volumeInfo.imageLinks.thumbnail,
+        description: book.volumeInfo.description,
+        /* , owner: id  */
+      });
     }
-}         
-   
+  }
 
-    
-    res.json(searchResult.data.items)
-})
+  res.json(searchResult.data.items);
+});
 
-
-
-module.exports = router
+module.exports = router;
